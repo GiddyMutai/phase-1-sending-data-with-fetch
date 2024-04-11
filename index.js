a95a94ce8a3aa47a40df4970a58e8e6d4afc3e86
Gideon Mutai <giddymutai13@gmail.com>
2024-04-11 12:35:31 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Add your code here
+// function to take in two arguments
+function submitData(userName, userEmail) {
+  // object to store the data
+  const data = {
+    name: userName,
+    email: userEmail,
+  };
+
+  // the fetch call that takes the destination URL and an object as the args
+  return (
+    fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      // handling the response
+      .then((response) => response.json())
+      .then((data) => {
+        document.body.innerHTML += `<p>${data.id}</p>`;
+      })
+
+      // catching the error
+      .catch((error) => {
+        document.body.innerHTML += `<p>${error.message}</p>`;
+      })
+  );
+}
+
+submitData("Gideon Mutai", "giddymutai13@gmail.com");
